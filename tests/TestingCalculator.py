be75ce543c3361d55036fa64fb86726e250ef878
M9SCO <chr.grigorii@gmail.com>
2021-10-20 15:08:26 +0600

    migrate TestingCalculator.py to sync

--- tests/TestingCalculator.py
+++ tests/TestingCalculator.py
@@ -1,41 +1,36 @@
-from aiounittest import AsyncTestCase, async_test
+from asyncio import run
+from unittest import TestCase
 from src import calculate
 
 
-class TesterCalculator(AsyncTestCase):
+class TesterCalculator(TestCase):
     "Проверка основной математической логики"
-    async def use_formula(self, f: str, expected: int):
-        self.assertEqual(await calculate(f, "../src/grammar_calculator.lark"), expected, f"{f}!={expected}")
+    def use_formula(self, f: str, expected: int):
+        self.assertEqual(run(calculate(f, "../src/grammar_calculator.lark")), expected, f"{f}!={expected}")
 
-    @async_test
-    async def testing_sum(self):
+    def testing_sum(self):
         "Должно корректно складывать"
-        await self.use_formula("1+1", 2)
+        self.use_formula("1+1", 2)
 
-    @async_test
-    async def testing_sub(self):
+    def testing_sub(self):
         "Должно корректно вычитать"
-        await self.use_formula("10-1", 9)
+        self.use_formula("10-1", 9)
 
-    @async_test
-    async def testing_mul(self):
+    def testing_mul(self):
         "Должно корректно умножать"
 
-        await self.use_formula("3*3", 9)
+        self.use_formula("3*3", 9)
 
-    @async_test
-    async def testing_dev(self):
+    def testing_dev(self):
         "Должно корректно делить"
-        await self.use_formula("10/2", 5)
+        self.use_formula("10/2", 5)
 
-    @async_test
-    async def testing_complex_math_expression(self):
+    def testing_complex_math_expression(self):
         "парсер должнен уметь обрабатывать комплексные выражения"
-        await self.use_formula("((2+2)*2+2+2*2)/2", 7)
+        self.use_formula("((2+2)*2+2+2*2)/2", 7)
 
-    @async_test
-    async def testing_wrong_formula(self):
+    def testing_wrong_formula(self):
         "парсер должен падать в ошибку, при непонятной формуле"
         with self.assertRaises(Exception):
             #ToDo Пофиксить на пробрасывание собственной ошибки
-            await self.use_formula("2?2", 0)
+            self.use_formula("2?2", 0)
