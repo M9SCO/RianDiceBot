2a96ab09b4d7ab1c46edd6118fdf68506d225839
M9SCO <chr.grigorii@gmail.com>
2021-10-22 21:53:58 +0600

    update parser module and his type hidings to use class Dice

--- src/parser.py
+++ src/parser.py
@@ -1,51 +1,46 @@
 from operator import add, mul, sub, truediv as div
-from random import randint
 
 from lark import Lark
-from lark.exceptions import UnexpectedCharacters
-
-from exceptions import ParseError
 
 __all__ = ["calculate"]
 
+from src.Dice import Dice
+
 
 async def simple_calculation(tree):
+    values = [await get_next_point(child) for child in tree.children]
     match tree.data:
         case "add":
-            f = add
+            return add(*values)
         case "sub":
-            f = sub
+            return sub(*values)
         case "mul":
-            f = mul
-        case _:
-            f = div
-    return f(*[await get_next_point(child) for child in tree.children])
+            return mul(*values)
+        case "div":
+            return div(*values)
 
 
-async def roll_dice(tree):
+async def parse_roll_dice(tree):
     if len(tree.children) == 2:
         thrown, face = [await get_next_point(child) for child in tree.children]
     else:
         thrown, face = 1, await get_next_point(*tree.children)
 
-    return [randint(1, face) for _ in range(thrown)]
-
+    return Dice(throw=thrown, face=face)
 
 async def filtration_dices(tree):
-    dices: list[int] = await get_next_point(tree.children[0])
-    lst = []
-    for i in range(await get_next_point(tree.children[1]) if len(tree.children) > 1 else 1):
-        if len(dices) == 0:
-            break
-        match tree.data:
-            case "max":
-                f = max
-            case "min":
-                f = min
-        res = f(dices)
-        lst.append(res)
-        dices.remove(res)
-    return lst
+    dice: Dice = await get_next_point(tree.children[0])
+
+    match tree.data:
+        case "max":
+            f = max
+        case "min":
+            f = min
+
+    dice._retain_f = f
+    dice._retain_n = await get_next_point(tree.children[1]) if len(tree.children) > 1 else 1
+    return dice
+
 
 
 async def get_next_point(tree):
@@ -57,7 +52,7 @@ async def get_next_point(tree):
         case "res":
             return sum([await get_next_point(child) for child in tree.children])
         case "dice":
-            return await roll_dice(tree)
+            return await parse_roll_dice(tree)
         case "repeat_sw":
             return [await get_next_point(tree.children[1]) for _ in range(await get_next_point(tree.children[0]))]
         case "repeat_ew":
@@ -67,17 +62,17 @@ async def get_next_point(tree):
 
 
 async def parsing(text, grammar):
-    try:
-        return await get_next_point(Lark(grammar, start="start").parse(text))
-    except UnexpectedCharacters as e:
-        raise ParseError(e)
+    return await get_next_point(Lark(grammar, start="start").parse(text, start="start"))
 
 
-async def calculate(text, path_to_grammar="grammar_calculator.lark"):
+async def calculate(text, path_to_grammar="src/grammar_calculator.lark"):
     with open(path_to_grammar) as f:
-        return await parsing(text, f.read())
+        grammar = f.read()
+
+    return await parsing(text, grammar)
 
 
-async def roll_dices(text, path_to_grammar):
+async def roll_dices(text, path_to_grammar = "src/grammar_dice.lark"):
     with open(path_to_grammar) as f:
-        return await parsing(text, f.read())
+        grammar = f.read()
+    return await parsing(text, grammar)
